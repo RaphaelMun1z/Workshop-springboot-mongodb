3add7d355ae72c930bcf881a701e8ea2d27279af
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-06-29 18:44:03 -0300

    feat: Criando entity Post com User aninhado

---
+++ src/main/java/com/raphaelmuniz/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.raphaelmuniz.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raphaelmuniz.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
